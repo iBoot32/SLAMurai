497824ce62270a408ad44f0fb913435bd8f26b05
Tom O'Donnell <tom.odonnell.udj@gmail.com>
2026-02-23 00:41:30 +0000

    re-organize and polish up codebase

--- autonav/madgwick_ros2_dashing/build/imu_madgwick_dashing/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/purduerm/ros2_ws/src/SLAMurai/autonav/madgwick_ros2_dashing
-# Build directory: /home/purduerm/ros2_ws/src/SLAMurai/autonav/madgwick_ros2_dashing/build/imu_madgwick_dashing
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
